a3603027690049a35a805af7838c99d471d9863f
Floris <Floris1996@hotmail.com>
2020-02-12 13:56:51 +0100

    adds configuration for the network

--- hello_world/index.js
+++ hello_world/index.js
@@ -3,8 +3,18 @@ const HelloTransaction = require('./hello_transaction');
 
 configDevnet.app.label = 'HelloWorld-blockchain-app';
 
-const app = new Application(genesisBlockDevnet, configDevnet);
+configDevnet.components.storage.database = 'lisk_dev2';
+//configDevnet.components.storage.database = 'lisk_dev';
+configDevnet.components.storage.host = 'bepiscoin.feddema.dev';
+configDevnet.components.storage.port = 5432;
+configDevnet.components.storage.user = 'lisk';
+configDevnet.components.storage.password = 'pass1234';
+configDevnet.modules.http_api.access.public = true;
 
+configDevnet.modules.network.seedPeers = [{ip: "144.91.116.43", wsPort: 5000}];
+
+
+const app = new Application(genesisBlockDevnet, configDevnet);
 app.registerTransaction(HelloTransaction);
 
 app
